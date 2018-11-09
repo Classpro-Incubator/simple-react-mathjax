20435c37b59578b17f5ba10c4563b119eb492c41
nikilarigela <nikil.arigela@gmail.com>
2018-11-09 13:49:35 +0530

    refract: code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MathJax extends Component {
+  constructor(props) {
+    super(props);
+    this.node = React.createRef();
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    this.renderMath();
+  }
+
+  componentDidUpdate() {
+    this.renderMath();
+  }
+
+  renderMath() {
+    window.MathJax.Hub.Queue([
+      "Typeset",
+      window.MathJax.Hub,
+      this.node.current
+    ]);
+  }
+
+  render() {
+    return (
+      <div className="App" ref={this.node}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default MathJax;
